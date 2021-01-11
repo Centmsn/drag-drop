b21c40399473d55138f1d631c6f81d02ab9cec6f
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-01-11 13:46:53 +0100

    add Autobind decorator

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,53 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+function Autobind(_, _2, descriptor) {
+    var originalMethod = descriptor.value;
+    var adjDescriptor = {
+        configurable: true,
+        get: function () {
+            var boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
         this.template = document.querySelector("#project-input");
         this.root = document.querySelector('#app');
         var content = document.importNode(this.template.content, true);
         this.element = content.firstElementChild;
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.configure();
         this.attach();
     }
+    ProjectInput.prototype.submitHandler = function (e) {
+        e.preventDefault();
+        console.log(this.titleInputElement.value);
+    };
+    ProjectInput.prototype.configure = function () {
+        this.element.addEventListener("submit", this.submitHandler);
+    };
     ProjectInput.prototype.attach = function () {
         this.root.insertAdjacentElement("afterbegin", this.element);
     };
+    __decorate([
+        Autobind,
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", [Event]),
+        __metadata("design:returntype", void 0)
+    ], ProjectInput.prototype, "submitHandler", null);
     return ProjectInput;
 }());
 var Input = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjDescriptor
+}
+
 class ProjectInput {
     template: HTMLTemplateElement
     root: HTMLElement
     element: HTMLElement
+    titleInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
 
     constructor() {
         this.template = document.querySelector("#project-input")! as HTMLTemplateElement;
@@ -9,8 +24,24 @@ class ProjectInput {
 
         const content = document.importNode(this.template.content, true)
         this.element = content.firstElementChild as HTMLElement
+        this.element.id = "user-input"
 
+        this.titleInputElement=this.element.querySelector("#title")! as HTMLInputElement
+        this.peopleInputElement=this.element.querySelector("#people")! as HTMLInputElement
+        this.descriptionInputElement=this.element.querySelector("#description")! as HTMLInputElement
+
+        this.configure()
         this.attach()
+    }
+
+    @Autobind
+    private submitHandler(e: Event) {
+        e.preventDefault();
+        console.log(this.titleInputElement.value)
+    }
+
+    private configure() {
+        this.element.addEventListener("submit", this.submitHandler)
     }
 
     private attach() {
